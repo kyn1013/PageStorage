d46f0e4f9e5e694ad813695332187e1c01727fa9
김예나 <yn112511@naver.com>
2024-04-30 23:59:11 +0900

    Update HistoryTagController.java

--- PageStorage/src/main/java/com/example/PageStorage/historytag/controller/HistoryTagController.java
+++ PageStorage/src/main/java/com/example/PageStorage/historytag/controller/HistoryTagController.java
@@ -5,11 +5,12 @@ import com.example.PageStorage.common.code.SuccessCode;
 import com.example.PageStorage.common.model.ResBodyModel;
 import com.example.PageStorage.historytag.service.HistoryTagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +24,16 @@ public class HistoryTagController {
         return PsResponse.toResponse(SuccessCode.SUCCES);
     }
 
+    //장고 서버로 보냄
+    @PostMapping("/sendText")
+    public ResponseEntity<String> sendText(@RequestBody String text) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8000/analyze"; // 장고 서버 URL
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(text, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+        return ResponseEntity.ok("Keywords received from Django: " + response.getBody());
+    }
 }
